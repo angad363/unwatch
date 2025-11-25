1723fada62a1d5b67843138f926f00a700b9fa2f
Angad Anil Gosain <angadgosain@gmail.com>
2025-11-25 09:53:53 +0530

    Refactored env variables.

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -17,12 +17,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClNIq71n2GOUbY7tQNUm6kFNeFl5BfO9M",
-  authDomain: "unwatch-479207.firebaseapp.com",
-  projectId: "unwatch-479207",
-  storageBucket: "unwatch-479207.firebasestorage.app",
-  messagingSenderId: "127526946808",
-  appId: "1:127526946808:web:f7c1404b3b1bd67591380d",
+  apiKey: process.env.FIREBASE_API_KEY!,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
